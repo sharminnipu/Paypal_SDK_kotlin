0fc3f6c9bc906b002cc82aa20c539f6327460fbb
JTL <nipusharmin41@gmail.com>
2021-11-08 17:00:28 +0600

    first commit

---
+++ app/src/main/java/com/sharminnipu/paypalintegration/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.sharminnipu.paypalintegration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.paypal.checkout.PayPalCheckout
+import com.paypal.checkout.approve.OnApprove
+import com.paypal.checkout.config.CheckoutConfig
+import com.paypal.checkout.config.Environment
+import com.paypal.checkout.config.SettingsConfig
+import com.paypal.checkout.createorder.CreateOrder
+import com.paypal.checkout.createorder.CurrencyCode
+import com.paypal.checkout.createorder.OrderIntent
+import com.paypal.checkout.createorder.UserAction
+import com.paypal.checkout.order.Amount
+import com.paypal.checkout.order.AppContext
+import com.paypal.checkout.order.Order
+import com.paypal.checkout.order.PurchaseUnit
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+
+
+    private val YOUR_CLIENT_ID="Acrrlef2uwvQoJ6wAiG30iLv9yXZplIABONc-IbYChg8bm9b5o_uJYvVFLhtxwzP_nK3qEWNObF9KqB9"
+    private  var amount:String="100.00"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val config = CheckoutConfig(
+            application = application,
+            clientId = YOUR_CLIENT_ID,
+            environment = Environment.SANDBOX,
+            returnUrl = "com.sharminnipu.paypalintegration://paypalpay",
+            currencyCode = CurrencyCode.USD,
+            userAction = UserAction.PAY_NOW,
+            settingsConfig = SettingsConfig(
+                loggingEnabled = true,
+                 false
+            )
+        )
+        PayPalCheckout.setConfig(config)
+
+
+
+          //  amount=etAmount.text.toString()
+
+        payPalButton.setup(
+            createOrder = CreateOrder { createOrderActions ->
+                val order = Order(
+                    intent = OrderIntent.CAPTURE,
+                    appContext = AppContext(
+                        userAction = UserAction.PAY_NOW
+                    ),
+                    purchaseUnitList = listOf(
+                        PurchaseUnit(
+                            amount = Amount(
+                                currencyCode = CurrencyCode.USD,
+                                value ="${amount}"
+                            )
+                        )
+                    )
+                )
+
+                createOrderActions.create(order)
+            },
+            onApprove = OnApprove { approval ->
+                approval.orderActions.capture { captureOrderResult ->
+                    Log.i("CaptureOrder", "CaptureOrderResult: $captureOrderResult")
+                }
+            }
+        )
+
+
+
+
+    }
+}
